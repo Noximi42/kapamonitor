806a0c0d1e043a945e62c800c6ac155c2e90509a
Fabian Pulch <contact@fabianpulch.com>
2020-04-19 09:49:15 +0200

    refactored offersOverview

--- app/client/src/containers/offersOverview/offersOverview.js
+++ app/client/src/containers/offersOverview/offersOverview.js
@@ -1,168 +1,70 @@
 import React, { useEffect } from "react";
-import TableContainer from "@material-ui/core/TableContainer";
-import Paper from "@material-ui/core/Paper";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import { TableCell, IconButton } from "@material-ui/core";
-import TableRow from "@material-ui/core/TableRow";
-import TableHead from "@material-ui/core/TableHead";
 import makeStyles from "@material-ui/core/styles/makeStyles";
-import LinearProgress from "@material-ui/core/LinearProgress";
-import Dialog from "@material-ui/core/Dialog";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import DialogActions from "@material-ui/core/DialogActions";
-import Button from "@material-ui/core/Button";
-import LocalHospitalIcon from "@material-ui/icons/LocalHospital";
-import HotelIcon from "@material-ui/icons/Hotel";
-import { getAllLocations } from "../../services/backend-rest-service";
-import PaddingLayout from "../../components/PaddingLayout";
-import { HospitalDetail } from "../../components/HospitalDetail";
-import { setRawLocations } from "../../store/leaflet/actions";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import PaddingLayout from '../../components/PaddingLayout';
+import CustomListItem from '../../components/CustomListItem';
 import { connect } from "react-redux";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import { Edit, Delete, Info } from "@material-ui/icons";
-import Checkbox from '@material-ui/core/Checkbox';
 
-export const headCells = [
-  { id: "type", label: "Typ", numberic: false },
-  { id: "title", label: "Name", numberic: false },
-  { id: "street", label: "Strasse", numberic: false },
-  { id: "postCode", label: "PLZ", numberic: true },
-  { id: "city", label: "Stadt", numberic: false },
-  { id: "numberOfBeds", label: "Anzahl Betten", numberic: true },
-  { id: "freeBeds", label: "Auslastung", numberic: true },
-];
-
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
-  tableRow: {
-    cursor: "pointer",
-  },
-});
-
-const getIconForType = (type) => {
-  switch (type) {
-    case "Hotel":
-      return <HotelIcon alt="Hotel"></HotelIcon>;
-      break;
-    case "Hospital":
-      return <LocalHospitalIcon alt="Krankenhaus"></LocalHospitalIcon>;
-      break;
-  }
-};
-
-const getNumberOfBedsForType = (row) => {
-  switch (row.type) {
-    case "Hotel":
-      return (
-        row.hotel.bedsWithVentilatorWithCarpet +
-        row.hotel.bedsWithoutVentilatorWithCarpet +
-        row.hotel.bedsWithVentilatorOtherFLoor
-      );
-      break;
-    case "Hospital":
-      return (
-        row.hospital.bedsWithVentilator + row.hospital.bedsWithoutVentilator
-      );
-      break;
-  }
-};
-const getCellContent = (row, cellId) => {
-  switch (cellId) {
-    case "street":
-      return `${row.street} ${row.houseNumber}`;
-      break;
-    case "type":
-      return getIconForType(row.type);
-      break;
-    case "numberOfBeds":
-      return getNumberOfBedsForType(row);
-      break;
-    case "freeBeds":
-      return (
-        <LinearProgress
-          variant="determinate"
-          value={row.capacity}
-        ></LinearProgress>
-      );
-    default:
-      return row[cellId];
-  }
-};
+const useStyles = makeStyles((theme) => ({}));
 
 const OffersOverview = (props) => {
   const classes = useStyles();
 
-  const [open, setOpen] = React.useState(false);
-  const [selectedRow, setSelectedRow] = React.useState(null);
+  const exampleData = [
+    {
+      id: 0,
+      ikId: "This some Test",
+      isEmergencyHospital: true,
+      bedsWithVentilator: 200,
+      bedsWithoutVentilator: 500,
+      barrierFree: true,
+      locationId: 0
+    },
+    {
+      id: 1,
+      ikId: "This some Testdata",
+      isEmergencyHospital: true,
+      bedsWithVentilator: 0,
+      bedsWithoutVentilator: 0,
+      barrierFree: true,
+      locationId: 1
+    },
+    {
+      id: 2,
+      ikId: "Ding 2",
+      isEmergencyHospital: true,
+      bedsWithVentilator: 0,
+      bedsWithoutVentilator: 0,
+      barrierFree: true,
+      locationId: 2
+    },
+  ]
 
-  useEffect(() => {
-    async function fetchRows() {
-      const res = await getAllLocations();
+  let rows = [];
 
-      if (res.status === 200) {
-        if (res.data.length > 0) {
-          const mockCapacity = res.data.map((location) => ({
-            ...location,
-            capacity: Math.floor(Math.random() * 100),
-          }));
-          props.setRawLocations(mockCapacity);
-        }
-      }
-    }
+  for(let item of exampleData) {
+    rows.push(
+      <CustomListItem></CustomListItem>
+    );
+  }
 
-    fetchRows();
-  }, []);
-
-  const [checked, setChecked] = React.useState(true);
-
-  const handleChange = (event) => {
-    setChecked(event.target.checked);
-  };
   return (
     <PaddingLayout title="Deine Angebote">
       <div className={classes.root}>
-        <Paper className={classes.paper} style={{ padding: 20 }}>
-          <Grid container>
-            <Grid item xs={12} sm={1}>
-              <Checkbox
-                checked={checked}
-                onChange={handleChange}
-                inputProps={{ "aria-label": "Check Item" }}
-              />
-            </Grid>
-            <Grid item xs={12} sm={3}>
-              <Typography variant="subtitle1">Artikelname</Typography>
-              <IconButton>
-                <Edit />
-              </IconButton>
-              <IconButton>
-                <Delete />
-              </IconButton>
-              <IconButton>
-                <Info />
-              </IconButton>
-            </Grid>
-            <Grid item xs={12} sm={8}>
-              <Typography variant="subtitle1">Zusatzinformationen</Typography>
-              <Typography variant="subtitle2">500 Stück</Typography>
-            </Grid>
-          </Grid>
-        </Paper>
+        {rows}
       </div>
     </PaddingLayout>
   );
 };
 
 const mapStateToProps = (state) => ({
-  rawLocations: state.leaflet.rawLocations,
+  // TODO StateToProps
+  // rawLocations: state.leaflet.rawLocations,
 });
 
 const mapDispatchToProps = {
-  setRawLocations,
+  // TODO Redux actions
+  // setRawLocations,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(OffersOverview);
