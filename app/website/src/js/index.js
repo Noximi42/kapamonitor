ef2968d5d35f92d9453a6a1f4d1d3bc03880d146
Christoph Desktop <christoph.biernoth@outlook.com>
2020-05-09 13:55:16 +0200

    Added content

--- app/website/src/js/index.js
+++ app/website/src/js/index.js
@@ -1,3 +1,19 @@
-import 'bootstrap';
+// --- style ---
 import '../scss/app.scss';
 import '../scss/index.scss';
+
+// --- assets ---
+import '../assets/logo.png';
+import '../assets/responsibility.png';
+import '../assets/solutionEnabler.png';
+
+import '../assets/team/timo.jpg'
+import '../assets/team/valentina.jpg'
+import '../assets/team/niroja.jpg'
+import '../assets/team/timo.jpg'
+
+
+// --- scripts ---
+import $ from "jquery";
+
+$('.carousel').carousel();
