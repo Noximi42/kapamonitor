cb603de72d116c1aedd71514d2f0bb743d0ee26f
Christoph Desktop <christoph.biernoth@outlook.com>
2020-05-13 00:41:11 +0200

    Removed facIcon (moved to index.html)

--- app/website/server.prod.js
+++ app/website/server.prod.js
@@ -1,8 +1,6 @@
 'use strict';
 
 const express = require('express');
-var favicon = require('serve-favicon');
-
 
 // Constants
 const PORT = 80;
@@ -11,8 +9,5 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.use(express.static('./dist/', {index: 'index.html'}))
-
-// FavIcon
-app.use(favicon('./dist/assets/favIcon.ico'));
 
 app.listen(PORT, HOST);
